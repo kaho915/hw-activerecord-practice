d237e5e56c7003d5802d5ca4dd61873d43053255
Kaho Furukawa <kaho.f@berkeley.edu>
2026-09-23 22:22:55 -0700

    changes saved

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -21,12 +21,70 @@ class Customer < ApplicationRecord
   #  You should NOT need to call Ruby library functions for sorting, filtering, etc.
 
   def self.any_candice
+    Customer.where(first: "Candice")
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+
   end
 
   def self.with_valid_email
+    Customer.where("email LIKE ?", "%@%")
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
   end
+
+  def self.with_dot_org_email
+    Customer.where("email LIKE ?", "%.org")
+  end
+
+  def self.with_invalid_email
+    Customer.where("email IS NOT NULL AND email != '' AND email NOT LIKE ?", "%@%")
+  end
+
+  def self.with_blank_email
+    Customer.where(email: [nil, ""])
+  end
+
+  def self.born_before_1980
+    Customer.where("birthdate < ?", Time.zone.parse("1 January 1980"))
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where("email LIKE ?", "%@%")
+    .where("birthdate < ?", Time.zone.parse("1 January 1980"))
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE ?", "B%").order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    Customer.order(birthdate: :desc).limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    Customer.where(first: "Gussie", last: "Murray")
+          .update_all(birthdate: Time.parse("February 8, 2004"))
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    Customer.where("email IS NOT NULL AND email != '' AND email NOT LIKE ?", "%@%")
+          .update_all(email: "")
+  end
+
+  def self.delete_meggie_herman
+    Customer.where(first: "Meggie", last: "Herman").destroy_all
+  end
+
+  def self.delete_everyone_born_before_1978
+    Customer.where("birthdate < ?", Time.zone.parse("1 January 1978"))
+          .destroy_all
+  end
+
+
+
+
+
+
+
   # etc. - see README.md for more details
 end
